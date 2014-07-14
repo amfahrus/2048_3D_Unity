a8239ffbe7774fd5b4f0821c951a2827c258ee7d
J. Reuben Wetherbee <jrweth@gmail.com>
2014-07-14 07:57:38 -0400

    bunch of changes / undo / etc.

--- Assets/_Scripts/GameControllerScript.cs
+++ Assets/_Scripts/GameControllerScript.cs
@@ -38,27 +38,31 @@ public class GameControllerScript : MonoBehaviour {
 
 	//instruction text
 	private string instructionText = @"The object of 2048-3D is to"
-		+ " move the blocks in such a way"
-			+ " that same numbered blocks collide"
+		+ " slide all the blocks in such a way"
+			+ " that blocks with the same numbers collide"
 			+ " forming a new block that is twice"
 			+ " as much as the originals until"
 			+ " the number 2048 is reached.\n\n"
+			
+			+ "To slide the blocks simply swipe up,"
+			+ " down, left or right (keyboard: arrow keys). To move the"
+			+ " blocks forward and backward use the" 
+			+ " big red arrow keys at the bottom of the screen (keyboard: a and z keys).  " 
+			+ " When moving, all blocks that can slide in the chosen direction will move"
+			+ " simultaneously, and any blocks moving toward a block with the same number will collide. "
+			+ " Two blocks with the number 0 will collide but never"
+			+ " increase higher than 0. You can turn"
+			+ " off 0s in the game options tab.\n\n"
+
 			+ "For every collision you receive"
 			+ " the number of points of the newly"
 			+ " created block. After each move is"
 			+ " made a new number (either 0, 2 or 4)"
 			+ " will be randomly assigned to an"
-			+ " empty block.  If after making a move"
+			+ " empty slot.  If after making a move"
 			+ " all blocks are filled and no new"
 			+ " moves are possible, the game ends. \n\n"
-			
-			+ "To move the blocks simply swipe up,"
-			+ " down, left or right. To move the"
-			+ " blocks forward and backward use the" 
-			+ " red arrow keys on screen.  Blocks"
-			+ " with 0 will combine but never"
-			+ " increase higher than 0. You can turn"
-			+ " off 0s in the game options tab.\n\n"
+
 			+ "2048-3D is based upon the original"
 			+ " 2048 game designed by Gabriele Cirulli.\n\n" 
 			+ "Soud effects by freeSFX http://www.freesfx.co.uk";
@@ -85,6 +89,9 @@ public class GameControllerScript : MonoBehaviour {
 					this.blocks[x,y,z] = blockInstance;
 					blockScript = blockInstance.gameObject.GetComponent("BlockScript") as BlockScript;
 					blockScript.originalPosition = new Vector3(x * this.scale, y * this.scale + this.yOffset, z * this.scale);
+					blockScript.x = x;
+					blockScript.y = y;
+					blockScript.z = z;
 					this.setBlockNumber(blockInstance, -1);
 					DontDestroyOnLoad(blockInstance);
 				}
@@ -141,17 +148,25 @@ public class GameControllerScript : MonoBehaviour {
 		return emptyBlocks;
 	}
 
-	private bool fillRandomBlock (){
+	private bool fillRandomBlock (ref int x, ref int y, ref int z, ref int newNumber) {
 		List<Transform> emptyBlocks = this.getEmptyBlocks();
 		int emptyIndex;
+		BlockScript blockScript; 
+
 		if (emptyBlocks.Count > 0) {
 			emptyIndex = Random.Range(0, emptyBlocks.Count);
 			if(this.optionsUse0) {
-				this.setBlockNumber(emptyBlocks[emptyIndex], Random.Range(0,3) * 2);
+				newNumber = Random.Range(0,3) * 2;
 			}
 			else {
+				newNumber = Random.Range(1,3) * 2;
 				this.setBlockNumber(emptyBlocks[emptyIndex], Random.Range(1,3) * 2);
 			}
+			this.setBlockNumber(emptyBlocks[emptyIndex], Random.Range(0,3) * 2);
+			blockScript = emptyBlocks[emptyIndex].GetComponent("BlockScript") as BlockScript;
+			x = blockScript.x;
+			y = blockScript.y;
+			z = blockScript.z;
 			return true;
 		}
 
@@ -169,6 +184,7 @@ public class GameControllerScript : MonoBehaviour {
 		Transform blockA, blockB, blockC;  //variables for holding the three blocks in a row
 		IDictionary<string, int> newNumbers = new Dictionary<string, int>(); //new numbers for blocks a,b & c to have
 		IDictionary<string, int> shiftBy = new Dictionary<string, int>(); //distance for blocks a,b & c to shift
+		int[,,] numbersAfterMove = new int[3,3,3];
 
 		//loop through each of the 9 rows to be calculated 
 		for(loop1 = 0; loop1 <=2; loop1++) {
@@ -226,6 +242,19 @@ public class GameControllerScript : MonoBehaviour {
 				if(newNumbers["a"] > this.getHighestBlock()) this.SetHighestBlock(newNumbers["a"]);
 				if(newNumbers["b"] > this.getHighestBlock()) this.SetHighestBlock(newNumbers["b"]);
 				if(newNumbers["c"] > this.getHighestBlock()) this.SetHighestBlock(newNumbers["c"]);
+
+				//save the numbers after the move for redo
+				numbersAfterMove[blockA.GetComponent<BlockScript>().x,
+				                 blockA.GetComponent<BlockScript>().y,
+				                 blockA.GetComponent<BlockScript>().z] = newNumbers["a"];
+				
+				numbersAfterMove[blockB.GetComponent<BlockScript>().x,
+				                 blockB.GetComponent<BlockScript>().y,
+				                 blockB.GetComponent<BlockScript>().z] = newNumbers["b"];
+				
+				numbersAfterMove[blockC.GetComponent<BlockScript>().x,
+				                 blockC.GetComponent<BlockScript>().y,
+				                 blockC.GetComponent<BlockScript>().z] = newNumbers["c"];
 			}
 		}
 		if (numMoves > 0) {
@@ -344,13 +373,15 @@ public class GameControllerScript : MonoBehaviour {
 
 
 	void  Update (){
+		int x =0, y=0, z=0, newNumber=0;
 		if(GameControllerScript.performRestart) {
 			this.restart ();
 			GameControllerScript.performRestart = false;
 		}
 		if(moveStartTime >= 0) {
 			if(Time.time - this.moveStartTime > this.moveDuration + 0.05F) {
-				this.fillRandomBlock();
+				this.fillRandomBlock(ref x, ref y, ref z, ref newNumber);
+				this.saveHistory ();
 				this.moveStartTime = -1F;
 				if(this.getEmptyBlocks().Count == 0) {
 					if(this.CheckGameOver()) {
@@ -367,6 +398,10 @@ public class GameControllerScript : MonoBehaviour {
 		if (Input.GetKeyUp("a")) this.doMove ("z", 1);
 		if (Input.GetKeyUp("z")) this.doMove ("z", -1);
 
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 	}
 
 	void FixedUpdate () {
@@ -380,18 +415,29 @@ public class GameControllerScript : MonoBehaviour {
 	}
 
 	public void restart() {
+		int[,,] positions = new int[3, 3, 3];
+		int x=0, y=0, z=0, newNumber=0;
 		this.gameLight.intensity = 4;
 		if(this.gameView == "game_over") { this.gameView = "game"; }
 
-		for (int x = 0; x <= 2; x++) {
-			for (int y = 0; y <= 2; y++) {
-				for (int z = 0; z <= 2; z++) {
+		for (x = 0; x <= 2; x++) {
+			for (y = 0; y <= 2; y++) {
+				for (z = 0; z <= 2; z++) {
 					this.setBlockNumber(this.blocks[x,y,z], -1);
+					positions[x,y,z] = -1;
 				}
 			}
 		}
-		this.fillRandomBlock();
-		this.fillRandomBlock();
+		PlayerPrefs.SetString ("redo_moves2","");
+		PlayerPrefs.SetString ("redo_moves1","");
+		PlayerPrefs.SetString ("redo_moves0","");
+		PlayerPrefs.SetInt ("redos", 2);
+		this.fillRandomBlock(ref x, ref y, ref z, ref newNumber);
+		positions [x, y, z] = newNumber;
+		this.fillRandomBlock(ref x, ref y, ref z, ref newNumber);
+		positions [x, y, z] = newNumber;
+		this.saveHistory();
+
 		this.setScore (0);
 	}
 
@@ -572,7 +618,9 @@ public class GameControllerScript : MonoBehaviour {
 		if (GUI.Button(new Rect(Screen.width * 2 / 3, Screen.height * 0.93F, Screen.width / 3, Screen.height * 0.06F), "Restart")) {
 			this.restart();
 		}
-
+		if (GUI.Button(new Rect(Screen.width * 2 / 3, Screen.height * 0.86F, Screen.width / 3, Screen.height * 0.06F), "Undo (" + PlayerPrefs.GetInt ("redos").ToString () + ")")) {
+			this.undo();
+		}
 	}
 
 	void ShowOptions() {
@@ -687,6 +735,53 @@ public class GameControllerScript : MonoBehaviour {
 				this.optionsPlaySounds = false;
 				this.previousOptionsPlaySounds = false;
 			}
+		}
+	}
+
+	private void saveHistory() {
+		int x,y,z;
+		string stringVal = "";
+
+		//create comma separated list of values
+		for (x = 0; x <= 2; x++) {
+			for (y = 0; y <= 2; y++) {
+				for (z = 0; z <= 2; z++) {
+					stringVal = stringVal + getBlockNumber(this.blocks[x,y,z]).ToString() + ",";
+				}
+			}
+		}
+		//strip off the last comma
+		stringVal = stringVal.Substring (0, stringVal.Length - 1);
+
+		PlayerPrefs.SetString ("redo_moves2",PlayerPrefs.GetString ("redo_moves1"));
+		PlayerPrefs.SetString ("redo_moves1",PlayerPrefs.GetString ("redo_moves0"));
+		PlayerPrefs.SetString ("redo_moves0",stringVal);
+
+		PlayerPrefs.Save();
+	}
+
+	private void undo() {
+		string[] positions = new string[27];
+		int positionNum = 0;
+		char[] separator = {','};
+
+		if( PlayerPrefs.GetString ("redo_moves1") != "" && PlayerPrefs.GetInt ("redos") > 0) {
+			positions = PlayerPrefs.GetString ("redo_moves1").Split (separator);
+			//loop through and set the block numbers for each block
+			for (int x = 0; x <= 2; x++) {
+				for (int y = 0; y <= 2; y++) {
+					for (int z = 0; z <= 2; z++) {
+						int.TryParse(positions[9*x+3*y+z], out positionNum);
+						this.setBlockNumber(this.blocks[x,y,z],positionNum);
+					}
+				}
+			}
+
+			//move all the redo moves back
+			PlayerPrefs.SetString ("redo_moves0",PlayerPrefs.GetString ("redo_moves1"));
+			PlayerPrefs.SetString ("redo_moves1",PlayerPrefs.GetString ("redo_moves2"));
+			PlayerPrefs.SetString ("redo_moves2","");
+			PlayerPrefs.SetInt ("redos", PlayerPrefs.GetInt ("redos") - 1);
 		}
 	}
 }

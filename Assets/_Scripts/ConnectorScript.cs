2c01fc09d3af91410df91fa937292bc84d313b3f
J. Reuben Wetherbee <jrweth@gmail.com>
2014-07-21 08:36:46 -0400

    added different game board options made empty blocks transparent made connector script to handle connector display

---
+++ Assets/_Scripts/ConnectorScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectorScript : MonoBehaviour {
+
+	public bool show;
+	// Use this for initialization
+	void Start () {
+		this.show = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.renderer.enabled = this.show;
+	}
+}
